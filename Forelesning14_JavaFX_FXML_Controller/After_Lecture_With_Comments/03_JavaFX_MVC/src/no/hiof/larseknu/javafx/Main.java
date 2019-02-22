8d83f59cef5b477c398d38740c61a8b9769043a3
Lars Emil Knudsen <larsemil87@gmail.com>
2019-02-22 09:10:17 +0100

    Oppdatert eksempel Forelesning 14

--- Forelesning14_JavaFX_FXML_Controller/After_Lecture_With_Comments/03_JavaFX_MVC/src/no/hiof/larseknu/javafx/Main.java
+++ Forelesning14_JavaFX_FXML_Controller/After_Lecture_With_Comments/03_JavaFX_MVC/src/no/hiof/larseknu/javafx/Main.java
@@ -10,14 +10,21 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        // Oppretter og instansierer et objekt av klassen FXMLLoader. Som vi benytter til å laste inn UI fra en .fxml fil.
         FXMLLoader fxmlInnlaster = new FXMLLoader();
+        // Setter lokasjonen vi skal laste inn .fxml filen fra (hvilken fil vi skal hente fra)
         fxmlInnlaster.setLocation(getClass().getResource("view/DyreDetalj.fxml"));
+        // Laster inn hovedLayoutet/rotnoden fra Hovedlayout.fxml
         Parent hovedLayout = fxmlInnlaster.load();
 
+        // Oppretter og instansierer scenen vi skal benytte, og setter hovedLayout(rotnoden), samt høyde og bredde
         Scene hovedScene = new Scene(hovedLayout, 400, 200);
 
+        // Legger scenen vi har laget til primaryStage (vinduet)
         primaryStage.setScene(hovedScene);
+        // Lager en tittel
         primaryStage.setTitle("Dyrene i Dyreparken");
+        // Viser primaryStage (vinduet). Uten denne vil vi ikke få frem noe på skjermen
         primaryStage.show();
     }
 
